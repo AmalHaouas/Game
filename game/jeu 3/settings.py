c65d952955c6155c7ef553fd74270d2eae4e5b21
AmalHaouas <150082837+AmalHaouas@users.noreply.github.com>
2023-11-06 22:38:29 +0100

    Add files via upload

---
+++ game/jeu 3/settings.py
@@ -0,0 +1,15 @@
+WHITE = (255, 255, 255)
+sgisalmon = (198, 113, 113)	
+BLACK = (0, 0, 0)
+DARKGREY = (40, 40, 40)
+LIGHTGREY = (100, 100, 100)
+BGCOLOUR = DARKGREY
+
+WIDTH = 1000
+HEIGHT = 800
+FPS = 70
+title = "Puzzle"
+TILESIZE = 128
+GAME_SIZE = 3
+
+
